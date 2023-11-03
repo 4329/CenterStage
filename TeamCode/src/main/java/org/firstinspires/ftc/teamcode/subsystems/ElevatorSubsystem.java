28c8a26405d70cea8011eeb7b4213bd80ebfe0b9
will <willhubb08@gmail.com>
2023-11-02 19:50:07 -0500

    wip work on elevator

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElevatorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElevatorSubsystem.java
@@ -7,15 +7,22 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class ElevatorSubsystem extends SubsystemBase {
+    private final double setPoint;
     private Motor elevatorMotor;
     private Telemetry telemetry;
 
     private static final double DISTANCEPERPULSE = 0.009335691828994;
+
     public ElevatorSubsystem(HardwareMap hm, Telemetry telemetry) {
         this.telemetry = telemetry;
         elevatorMotor = new Motor(hm, "elevatorMotor");
         elevatorMotor.setDistancePerPulse(DISTANCEPERPULSE);
-        elevatorMotor.setRunMode(Motor.RunMode.RawPower);
+        this.elevatorMotor.setRunMode(Motor.RunMode.PositionControl);
+        this.elevatorMotor.setPositionCoefficient(0.5);
+        this.elevatorMotor.setPositionTolerance(0.314159265358979);
+        this.elevatorMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.setPoint = 0;
+        this.elevatorMotor.encoder.reset();
 
     }
 
