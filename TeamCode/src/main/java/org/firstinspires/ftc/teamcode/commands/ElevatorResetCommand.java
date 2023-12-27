9a60129a69298a275313385aad9b48f6ac04b041
khaiwall <98569493+khaiwall@users.noreply.github.com>
2023-12-27 16:13:47 -0600

    made and completed elevator zero command (#10)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/ElevatorResetCommand.java
@@ -0,0 +1,83 @@
+package org.firstinspires.ftc.teamcode.commands;
+
+import android.util.Log;
+
+import com.arcrobotics.ftclib.command.CommandBase;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.ElevatorSubsystem;
+
+public class ElevatorResetCommand extends CommandBase {
+
+    private final ElevatorSubsystem elevatorSubsystem;
+    private final Telemetry telemetry;
+    private boolean downed = false;
+    private boolean done = false;
+
+
+    public ElevatorResetCommand(ElevatorSubsystem elevatorSubsystem, Telemetry telemetry) {
+
+        this.elevatorSubsystem = elevatorSubsystem;
+        this.telemetry = telemetry;
+
+    }
+
+    @Override
+    public void initialize() {
+
+        Log.i("elevatorReset", "INITIALIZE1234567890");
+        elevatorSubsystem.switchPower();
+        downed = false;
+        done = false;
+
+    }
+
+    @Override
+    public void execute() {
+        if (!downed) {
+
+            Log.i("elevatorReset", "1NOT downed");
+            if (!elevatorSubsystem.isDown()) {
+                Log.i("elevatorReset", "2elevatorSubsystem NOT down");
+
+                elevatorSubsystem.goDown();
+            } else {
+                Log.i("elevatorReset", "3elevatorSubsystem IS down");
+
+                downed = true;
+                elevatorSubsystem.stop();
+
+            }
+        } else {
+            Log.i("elevatorReset", "4IS downed");
+
+            if (elevatorSubsystem.isDown()) {
+                Log.i("elevatorReset", "5elevatorSubsystem IS downDowned");
+
+                elevatorSubsystem.goUp();
+            } else {
+
+                Log.i("elevatorReset", "6elevatorSubsystem IS !DownedbutUP");
+
+                elevatorSubsystem.stop();
+                done = true;
+
+            }
+        }
+
+
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Log.i("elevatorReset", "ENDENDEND");
+        elevatorSubsystem.switchPosition();
+
+    }
+
+    @Override
+    public boolean isFinished() {
+
+        return done;
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElevatorSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ElevatorSubsystem.java
@@ -3,29 +3,26 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.arcrobotics.ftclib.command.SubsystemBase;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.util.ElevatorPosition;
+import org.firstinspires.ftc.teamcode.util.MathUtil;
 
 public class ElevatorSubsystem extends SubsystemBase {
     private int setPoint;
     private Motor elevatorMotor;
     private Telemetry telemetry;
+    private TouchSensor elevatorSensor;
+    private boolean positionControl = true;
 
     private static final double DISTANCEPERPULSE = 0.009335691828994;
 
     public ElevatorSubsystem(HardwareMap hm, Telemetry telemetry) {
         this.telemetry = telemetry;
         elevatorMotor = new Motor(hm, "elevatorMotor");
-        elevatorMotor.setDistancePerPulse(DISTANCEPERPULSE);
-        this.elevatorMotor.setInverted(true);
-        this.elevatorMotor.setRunMode(Motor.RunMode.PositionControl);
-        this.elevatorMotor.setPositionCoefficient(1);
-        this.elevatorMotor.setFeedforwardCoefficients(0,0.35);
-        this.elevatorMotor.setPositionTolerance(0.5);
-        this.elevatorMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
-        this.setPoint = 0;
-        this.elevatorMotor.encoder.reset();
+        switchPosition();
+        this.elevatorSensor = hm.get(TouchSensor.class, "elevatorSensor");
 
     }
 
@@ -43,10 +40,18 @@ public class ElevatorSubsystem extends SubsystemBase {
 
     @Override
     public void periodic() {
-        if(this.elevatorMotor!=null){
-        this.elevatorMotor.set(0.5);
+
+        telemetry.addLine("the elevator is pressed?" + elevatorSensor.isPressed());
+
+        if(this.elevatorMotor!=null && positionControl){
+           this.elevatorMotor.set(0.5);
+
         }
+
     }
+
+
+
 
     public void stop() {
 
@@ -92,4 +97,40 @@ public class ElevatorSubsystem extends SubsystemBase {
         return elevatorMotor.atTargetPosition();
 
         }
+
+    public boolean isDown() {
+        telemetry.addLine("ELEVATORISDOWN" + elevatorSensor.isPressed());
+        return  elevatorSensor.isPressed();
+    }
+
+    public void goDown() {
+        elevatorMotor.set(-0.5);
+    }
+
+    public void goUp() {
+
+        elevatorMotor.set(0.2);
+
+    }
+
+    public void switchPower() {
+        this.elevatorMotor.setRunMode(Motor.RunMode.RawPower);
+        this.positionControl = false;
+    }
+
+    public void switchPosition() {
+
+        elevatorMotor.setDistancePerPulse(DISTANCEPERPULSE);
+        this.elevatorMotor.setInverted(true);
+        this.elevatorMotor.setRunMode(Motor.RunMode.PositionControl);
+        this.elevatorMotor.setPositionCoefficient(1);
+        this.elevatorMotor.setFeedforwardCoefficients(0,0.35);
+        this.elevatorMotor.setPositionTolerance(0.5);
+        this.elevatorMotor.setZeroPowerBehavior(Motor.ZeroPowerBehavior.BRAKE);
+        this.setPoint = 0;
+        this.elevatorMotor.encoder.reset();
+        this.positionControl = true;
+    }
+
+
 }
