ff37520449417a077f29ef1c2a024f03df3c4dc0
khaiwall <98569493+khaiwall@users.noreply.github.com>
2023-12-27 13:42:20 -0600

    made a slow speed (#9)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/MecanumDriveCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/MecanumDriveCommand.java
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.commands;
 
 import com.arcrobotics.ftclib.command.CommandBase;
-import com.qualcomm.robotcore.robocol.Command;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.subsystems.MecanumDriveSubsystem;
@@ -15,44 +14,53 @@ public class MecanumDriveCommand extends CommandBase {
     private DoubleSupplier rotateDrive;
     private DoubleSupplier strafeDrive;
     private BooleanSupplier speedBooost;
+    private BooleanSupplier slowBoost;
     private Telemetry telemetry;
-    private static double SLOW_MOTION_DIVISOR=3.0;
+    private static double REGULAR_MOTION_DIVISOR = 3.0;
+    private static double SLOW_MOTION_DIVISOR = 6.0;
 
     public MecanumDriveCommand(MecanumDriveSubsystem mecanumDriveSubsystem,
                                DoubleSupplier forwardDrive,
                                DoubleSupplier rotateDrive,
                                DoubleSupplier strafeDrive,
                                BooleanSupplier speedBooost,
+                               BooleanSupplier slowBoost,
                                Telemetry telemetry) {
         this.mecanumDriveSubsystem = mecanumDriveSubsystem;
         this.forwardDrive = forwardDrive;
         this.rotateDrive = rotateDrive;
         this.strafeDrive = strafeDrive;
         this.speedBooost = speedBooost;
+        this.slowBoost = slowBoost;
         this.telemetry = telemetry;
         addRequirements(mecanumDriveSubsystem);
     }
 
     @Override
     public void initialize() {
-        telemetry.speak("speedBooost");
+
     }
 
     @Override
     public void execute() {
-        telemetry.addData("speedBooost",speedBooost.getAsBoolean());
+        telemetry.addData("speedBooost", speedBooost.getAsBoolean());
         if (speedBooost.getAsBoolean()) {
             mecanumDriveSubsystem.drive(
                     forwardDrive.getAsDouble(),
                     rotateDrive.getAsDouble(),
                     strafeDrive.getAsDouble());
-        }
-        else {
+        } else if (slowBoost.getAsBoolean()) {
+            mecanumDriveSubsystem.drive(
+                    forwardDrive.getAsDouble() / SLOW_MOTION_DIVISOR,
+                    rotateDrive.getAsDouble() / SLOW_MOTION_DIVISOR,
+                    strafeDrive.getAsDouble() / SLOW_MOTION_DIVISOR);
+
+        } else {
 
             mecanumDriveSubsystem.drive(
-                    forwardDrive.getAsDouble()/SLOW_MOTION_DIVISOR,
-                    rotateDrive.getAsDouble()/SLOW_MOTION_DIVISOR,
-                    strafeDrive.getAsDouble()/SLOW_MOTION_DIVISOR);
+                    forwardDrive.getAsDouble() / REGULAR_MOTION_DIVISOR,
+                    rotateDrive.getAsDouble() / REGULAR_MOTION_DIVISOR,
+                    strafeDrive.getAsDouble() / REGULAR_MOTION_DIVISOR);
         }
     }
 }
